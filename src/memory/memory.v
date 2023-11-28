0b339c81e065949d64bc7126d335c9ca05edb036
sprsr <projecthourglass@protonmail.com>
2023-11-27 22:34:59 -0600

    mroe

--- src/memory/memory.v
+++ src/memory/memory.v
@@ -6,14 +6,21 @@ module memory(
     parameter DIRTY_BITS = 1,
     parameter TAG_BITS = 18,
     parameter DATA_WIDTH = 32)
+
 (
-    input  [(TAG_BITS -1): 0]            tag,
-    input  [($log2(CACHE_LINES) - 1): 0] index,
-    output [(DATA_WIDTH -1): 0]          data,
-    output                               cache_hit
+    input                                clk,
+    input                                rst,
+    input  [(TAG_BITS -1): 0]            i_tag,
+    input  [($log2(CACHE_LINES) - 1): 0] i_index,
+    output [(DATA_WIDTH -1): 0]          o_data,
+    output                               o_cache_hit
 );
 
 
 reg cache [((VALID_BITS + LRU_BITS + DIRTY_BITS + TAG_BITS + (LINE_SIZE_BYTES * 8)) * CACHE_LINES) - 1 : 0];
+
+always @(posedge clk or posedge rst) begin
+    if 
+
 
 
