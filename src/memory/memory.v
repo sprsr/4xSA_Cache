b77f41ed927deb1a169a0c9c380af734f4f1c8a8
sprsr <projecthourglass@protonmail.com>
2023-11-27 22:30:50 -0600

    memory parameters

--- src/memory/memory.v
+++ src/memory/memory.v
@@ -4,17 +4,15 @@ module memory(
     parameter LRU_BITS = 1,
     parameter VALID_BITS = 1,
     parameter DIRTY_BITS = 1,
-    parameter TAG_BITS = 18)
-    input tag,
-    input index,
-)
+    parameter TAG_BITS = 18,
+    parameter DATA_WIDTH = 32)
+(
+    input  [(TAG_BITS -1): 0]            tag,
+    input  [($log2(CACHE_LINES) - 1): 0] index,
+    output [(DATA_WIDTH -1): 0]          data,
+    output                               cache_hit
+);
 
-parameter CACHE_LINES = 256,
-          LINE_SIZE_BYTES   = 64,
-          LRU_BITS = 1,
-          VALID_BITS = 1,
-          DIRTY_BITS = 1,
-          TAG_BITS = 18;
 
 reg cache [((VALID_BITS + LRU_BITS + DIRTY_BITS + TAG_BITS + (LINE_SIZE_BYTES * 8)) * CACHE_LINES) - 1 : 0];
 
