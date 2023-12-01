affd15076fb474cca201bd88e9d194a4c11e5569
sprsr <projecthourglass@protonmail.com>
2023-11-30 23:55:20 -0600

    memory seems to compile

--- src/memory/memory.v
+++ src/memory/memory.v
@@ -6,28 +6,23 @@ module memory
     parameter LRU_BITS = 1,
     parameter VALID_BITS = 1,
     parameter DIRTY_BITS = 1,
+    parameter TAG_BITS = 18,
     parameter DATA_WIDTH = 32,
     parameter ADDRESS_WIDTH = 32,
-    parameter LINE_SIZE_BITS = LINE_SIZE_BYTES*8,
-    parameter TAG_BITS = ADDRESS_WIDTH - (INDEX_WIDTH + OFFSET_WIDTH),
-    parameter LINE_WIDTH = (VALID_BITS + LRU_BITS + DIRTY_BITS + TAG_BITS + (LINE_SIZE_BYTES * 8)),
     parameter WAYS = 4)
 
     (
-        input                                clk,
-        input                                rst,
-        input  [ADDRESS_WIDTH -1 : 0]        i_address,
-        input  [($log2(CACHE_LINES) - 1): 0] i_index,
-        output [(DATA_WIDTH -1): 0]          o_data,
+        input                                    clk,
+        input                                    rst,
+        input  [TAG_BITS -1 : 0]                 i_tag,
+        input  [($log2(CACHE_LINES) - 1): 0]     i_index,
+        input  [($log2(LINE_SIZE_BYTES) - 1): 0] i_offset,
+        output [(DATA_WIDTH -1): 0]              o_data,
         output                               o_cache_hit
     );
 
-    localparam OFFSET_WIDTH  = $log2(LINE_SIZE_BYTES);
-    localparam INDEX_WIDTH   = $log2(CACHE_LINES);
-    localparam ADDRESS_TAG = i_address[(ADDRESS_WIDTH - 1) -: TAG_BITS];
-    localparam ADDRESS_INDEX = i_address[ADDRESS_WIDTH - TAG_BITS - 1 -: INDEX_WIDTH];
-    localparam ADDRESS_OFFSET = i_address[ADDRESS_WIDTH - TAG_BITS - INDEX_WIDTH - 1 -: OFFSET_WIDTH];
-
+    localparam LINE_SIZE_BITS = LINE_SIZE_BYTES * 8;
+    localparam LINE_WIDTH = (VALID_BITS + LRU_BITS + DIRTY_BITS + TAG_BITS + (LINE_SIZE_BYTES * 8));
 
     reg [LINE_WIDTH - 1 : 0] cache [0:(CACHE_LINES - 1)] [0: WAYS - 1];
     wire [WAYS - 1 : 0] hit;
@@ -36,7 +31,7 @@ module memory
 
     initial begin 
         for (i = 0; i < WAYS; i= i+1) begin
-            data[i] = cache[ADDRESS_INDEX][i][LINE_SIZE_BITS - 1: 0];
+            data[i] = cache[i_index][i][LINE_SIZE_BITS - 1: 0];
         end
     end
 
@@ -46,13 +41,13 @@ module memory
 
         for (i = 0; i < WAYS; i= i+1) begin
             Comparator #() u_inst_comparator (
-                .i_a(cache[ADDRESS_INDEX][i][(LINE_WIDTH - VALID_BITS - LRU_BITS - DIRTY_BITS - 1) -: TAG_BITS]),
-                .i_b(ADDRESS_TAG),
+                .i_a(cache[i_index][i][(LINE_WIDTH - VALID_BITS - LRU_BITS - DIRTY_BITS - 1) -: TAG_BITS]),
+                .i_b(i_tag),
                 .o_y(hit[i])
             );
             AND #() u_inst_and (
                 .i_a(hit[i]),
-                .i_b(cache[ADDRESS_INDEX][i][LINE_WIDTH - 1]),
+                .i_b(cache[i_index][i][LINE_WIDTH - 1]),
                 .o_y(mux_sel[i])
             );
         end
