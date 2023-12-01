bfc2bc3b06a2734e79b2d6391c4ed2f062a2cb77
sprsr <projecthourglass@protonmail.com>
2023-11-30 23:06:07 -0600

    adding io

--- src/cache_controller/cache_controller.v
+++ src/cache_controller/cache_controller.v
@@ -4,10 +4,18 @@ module cache_controller#(
     parameter LRU_BITS = 1,
     parameter VALID_BITS = 1,
     parameter DIRTY_BITS = 1,
-    parameter TAG_BITS = 18,
     parameter DATA_WIDTH = 32,
-    parameter WAYS = 4)
-)
+    parameter WAYS = 4,
+    parameter OFFSET_WIDTH  = $log2(LINE_SIZE_BYTES),
+    parameter INDEX_WIDTH   = $log2(CACHE_LINES),
+    parameter TAG_BITS = 18,
+    parameter ADDRESS_WIDTH = TAG_BITS + INDEX_WIDTH + OFFSET_WIDTH)
+(
     input      clk,
     input      rst,
-    input
+    input      [ADDRESS_WIDTH - 1 : 0] i_address,
+    input      [INDEX_WIDTH - 1: 0]    i_index,
+    output [(DATA_WIDTH -1): 0]          o_data,
+    output                               o_cache_hit
+);
+    
