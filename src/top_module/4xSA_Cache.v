be3d44cb5c5fde7a76bada7ccb5a7e689e788c91
sprsr <projecthourglass@protonmail.com>
2023-12-02 01:09:07 -0600

    adding top level cache module and instantiating cache_controller

--- src/top_module/4xSA_Cache.v
+++ src/top_module/4xSA_Cache.v
@@ -1,4 +1,22 @@
-module 4xSA_Cache(
-    input clk,
-    input rst,
+module 4xSA_Cache
+#(  parameter CACHE_LINES = 256,
+    parameter LINE_SIZE_BYTES   = 64,
+    parameter LRU_BITS = 1,
+    parameter VALID_BITS = 1,
+    parameter DIRTY_BITS = 1,
+    parameter TAG_BITS = 18,
+    parameter INDEX_BITS = 8,
+    parameter OFFSET_BITS = 6,
+    parameter DATA_WIDTH = 32,
+    parameter ADDRESS_WIDTH = 32,
+    parameter WAYS = 4)
+
+(
+    input         clk,
+    input         rst,
+    input  [31:0] i_addr,
+    input  [31:0]  dataW,
+    input          memRW,
+    output [31:0] o_data
 );
+
