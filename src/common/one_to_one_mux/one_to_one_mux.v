a1a83a5439dbbdaa0713c0d668ab772b541ced27
sprsr <projecthourglass@protonmail.com>
2023-12-01 23:49:37 -0600

    changed one_to_one_mux.v

--- src/common/one_to_one_mux/one_to_one_mux.v
+++ src/common/one_to_one_mux/one_to_one_mux.v
@@ -1,21 +1,21 @@
 // 1-to-1 Multiplexer Module
 
 module one_to_one_mux 
-#( parameter DATA_WIDTH = 32,
-   parameter INPUTS     = 4)
+#( parameter LINE_SIZE_BYTES = 32,
+   parameter WAYS     = 4)
 (
-  input wire  [DATA_WIDTH - 1 : 0] i_data [INPUTS - 1: 0],   
-  input wire  [INPUTS - 1: 0]      i_sel,
-  output wire [DATA_WIDTH - 1 : 0] o_y 
+  input wire  [(LINE_SIZE_BYTES*8) - 1 : 0] i_data [WAYS - 1: 0],   
+  input wire  [WAYS - 1: 0]      i_sel,
+  output wire [(LINE_SIZE_BYTES*8) - 1 : 0] o_y 
 );
-reg [DATA_WIDTH - 1 : 0] r_y = 'h0;
+reg [LINE_SIZE_BYTES - 1 : 0] r_y = 'h0;
 
-generate
-    genvar i;
-    for (i = 0; i < INPUTS; i = i+1) begin
+always @(*) begin
+    for (i = 0; i < WAYS; i = i+1) begin
         if (i_sel[i]) begin
             assign r_y = i_data[i];
         end
     end
-endgenerate
+end
+
 endmodule
