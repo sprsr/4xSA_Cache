7fb5fe051d8a7e6c4240fb164cfdbddab4fdb2e9
sprsr <projecthourglass@protonmail.com>
2023-12-02 00:29:07 -0600

    one_to_one_mux updates

--- src/common/one_to_one_mux/one_to_one_mux.v
+++ src/common/one_to_one_mux/one_to_one_mux.v
@@ -8,14 +8,14 @@ module one_to_one_mux
   input wire  [WAYS - 1: 0]      i_sel,
   output wire [(LINE_SIZE_BYTES*8) - 1 : 0] o_y 
 );
+
 reg [LINE_SIZE_BYTES - 1 : 0] r_y = 'h0;
 
 always @(*) begin
-    for (i = 0; i < WAYS; i = i+1) begin
+    for (int i = 0; i < WAYS; i = i+1) begin
         if (i_sel[i]) begin
             assign r_y = i_data[i];
         end
     end
 end
-
 endmodule
