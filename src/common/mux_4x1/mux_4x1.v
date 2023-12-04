1e7cc66775a18512fd9e2df818a7656215b4e391
sprsr <projecthourglass@protonmail.com>
2023-12-03 21:56:21 -0600

    new file for mux

---
+++ src/common/mux_4x1/mux_4x1.v
@@ -0,0 +1,20 @@
+module mux_4x_1 
+#( parameter LINE_SIZE_BYTES = 64)
+(
+  input wire  [(LINE_SIZE_BYTES*8) - 1 : 0] i_data [3: 0],   
+  input wire  [3: 0]      i_sel,
+  output wire [(LINE_SIZE_BYTES*8) - 1 : 0] o_y 
+);
+
+reg [LINE_SIZE_BYTES - 1 : 0] r_y;
+assign o_y = r_y;
+
+always @(*) begin
+    case (i_sel) 
+        4'b0001: r_y <= i_data[0];
+        4'b0010: r_y <= i_data[1];
+        4'b0100: r_y <= i_data[2];
+        4'b1000: r_y <= i_data[3];
+    endcase
+end
+endmodule
--- src/common/one_to_one_mux/one_to_one_mux.v
+++ src/common/one_to_one_mux/one_to_one_mux.v
@@ -10,11 +10,11 @@ module one_to_one_mux
 );
 
 reg [LINE_SIZE_BYTES - 1 : 0] r_y;
-reg [$clog2(WAYS):0] selector;
 assign o_y = r_y;
 
 
-function [$clog2(WAYS):0] this_way(input [WAYS-1:0] select);
+//reg [$clog2(WAYS):0] selector;
+/*function [$clog2(WAYS):0] this_way(input [WAYS-1:0] select);
     for (integer i = 0; i < WAYS; i = i+1) begin
         if (select[i]) begin
             this_way <= i;
@@ -22,14 +22,17 @@ function [$clog2(WAYS):0] this_way(input [WAYS-1:0] select);
         end
     end
     this_way = WAYS;
-endfunction
+endfunction*/
 
 always @(*) begin
     //for (integer i = 0; i < WAYS; i = i+1) begin
-    selector = this_way(i_sel);
-    if (selector < WAY)
-        r_y <= i_data[selector];
-    else 
-        r_y <= 'h0;
+    //selector = this_way(i_sel);
+    //if (selector < WAY)
+    case (i_sel) begin
+        4'h1: r_y <= i_data[0];
+        h'2: r_y <= i_data[1];
+        h'4: r_y <= i_data[2];
+        h'8: r_y <= i_data[3];
+    endcase
 end
 endmodule
