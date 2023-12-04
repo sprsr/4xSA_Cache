e72337501ba95b5a23ac8dba8596e0dcb652a5e4
sprsr <projecthourglass@protonmail.com>
2023-12-03 21:24:12 -0600

    one to one mux

--- src/common/one_to_one_mux/one_to_one_mux.v
+++ src/common/one_to_one_mux/one_to_one_mux.v
@@ -9,14 +9,27 @@ module one_to_one_mux
   output wire [(LINE_SIZE_BYTES*8) - 1 : 0] o_y 
 );
 
-reg [LINE_SIZE_BYTES - 1 : 0] r_y = 'h0;
+reg [LINE_SIZE_BYTES - 1 : 0] r_y;
+reg [$clog2(WAYS):0] selector;
 assign o_y = r_y;
 
-always @(*) begin
-    for (int i = 0; i < WAYS; i = i+1) begin
-        if (i_sel[i]) begin
-            r_y = i_data[i];
+
+function [$clog2(WAYS):0] this_way(input [WAYS-1:0] select);
+    for (integer i = 0; i < WAYS; i = i+1) begin
+        if (select[i]) begin
+            this_way <= i;
+            return;
         end
     end
+    this_way = WAYS;
+endfunction
+
+always @(*) begin
+    //for (integer i = 0; i < WAYS; i = i+1) begin
+    selector = this_way(i_sel);
+    if (selector < WAY)
+        r_y <= i_data[selector];
+    else 
+        r_y <= 'h0;
 end
 endmodule
