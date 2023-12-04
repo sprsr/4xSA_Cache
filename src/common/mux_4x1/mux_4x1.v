db01be36450c9f7554a8da94f0075083934860fe
sprsr <projecthourglass@protonmail.com>
2023-12-03 23:11:30 -0600

    4x1 mux

--- src/common/mux_4x1/mux_4x1.v
+++ src/common/mux_4x1/mux_4x1.v
@@ -1,12 +1,12 @@
 module mux_4x_1 
-#( parameter LINE_SIZE_BYTES = 64)
+#( parameter LINE_SIZE_BITS = 512)
 (
-  input wire  [(LINE_SIZE_BYTES*8) - 1 : 0] [3: 0] i_data ,   
-  input wire  [3: 0]      i_sel,
-  output wire [(LINE_SIZE_BYTES*8) - 1 : 0] o_y 
+  input [LINE_SIZE_BITS - 1 : 0][3: 0]  i_data,   
+  input [3: 0]      i_sel,
+  output [LINE_SIZE_BITS - 1 : 0] o_y 
 );
 
-reg [LINE_SIZE_BYTES - 1 : 0] r_y;
+reg [LINE_SIZE_BITS - 1 : 0] r_y;
 assign o_y = r_y;
 
 always @(*) begin
