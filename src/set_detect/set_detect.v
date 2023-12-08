cee7bf10e87b3fcc8644ad862f4e4225ac2bf060
sprsr <projecthourglass@protonmail.com>
2023-12-08 16:32:45 -0600

    updated set_detect

--- src/set_detect/set_detect.v
+++ src/set_detect/set_detect.v
@@ -10,5 +10,8 @@ assign set = r_set;
 always @(*) begin
     case (hit)
         4'b0000: r_set <= 0;
+        4'b0010: r_set <= 1;
+        4'b0000: r_set <= 2;
+        4'b0000: r_set <= 3;
 end
 endmodule
