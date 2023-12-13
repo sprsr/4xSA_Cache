fbcc0d0b78a243c4d196dd4dbc955f2fb040c866
sprsr <projecthourglass@protonmail.com>
2023-12-12 21:40:27 -0600

    editing tb

--- tb/sa_cache/sa_cache_tb.v
+++ tb/sa_cache/sa_cache_tb.v
@@ -4,11 +4,11 @@ module sa_cache_tb;
     //reset that pulses once
     reg reset = 0;
     reg clk;
-    reg i_tag_in;
-    reg i_index_in;
-    reg i_offset_in;
-    reg dataW_in;
-    reg i_memory_line_in;
+    reg [17:0] i_tag_in;
+    reg [7:0]  i_index_in;
+    reg [5:0]  i_offset_in;
+    reg [31:0] dataW_in;
+    reg [52:0 ]i_memory_line_in;
     reg i_memory_response_in;
     reg memRW_in;
     reg o_data_out;
@@ -24,6 +24,7 @@ module sa_cache_tb;
 
     initial begin
         i_tag_in = 0;
+        i_index_in <= 
         forever #10 = 
     initial begin
 
