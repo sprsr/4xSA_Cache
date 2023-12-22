5214abaeeb2eef1d305a0baec46e54cb817f7d7c
sprsr <projecthourglass@protonmail.com>
2023-12-22 00:34:52 -0600

    mux_4x1

--- src/common/mux_4x1/mux_4x1.v
+++ src/common/mux_4x1/mux_4x1.v
@@ -1,7 +1,13 @@
 module mux_4x_1 
-#( parameter LINE_SIZE_BITS = 512)
+#( 
+   parameter LINE_SIZE_BYTES = 4,
+   parameter LRU_BITS = 1,
+   parameter VALID_BITS = 1,
+   parameter DIRTY_BITS = 1,
+   parameter TAG_BITS = 18,
+)
 (
-  input [LINE_SIZE_BITS - 1 : 0][3: 0]  i_data,   
+  input [(VALID_BITS + LRU_BITS + DIRTY_BITS + TAG_BITS + (LINE_SIZE_BYTES * 8))- 1 : 0]  i_data [0: WAYS-1],
   input [3: 0]      i_sel,
   output [LINE_SIZE_BITS - 1 : 0] o_y 
 );
@@ -11,10 +17,10 @@ assign o_y = r_y;
 
 always @(*) begin
     case (i_sel) 
-        4'b0001: r_y <= i_data[0];
-        4'b0010: r_y <= i_data[1];
-        4'b0100: r_y <= i_data[2];
-        4'b1000: r_y <= i_data[3];
+        4'b0001: r_y <= i_data[0][(LINE_SIZE_BYTES*8) -1 :0];
+        4'b0010: r_y <= i_data[1][(LINE_SIZE_BYTES*8) -1 :0];
+        4'b0100: r_y <= i_data[2][(LINE_SIZE_BYTES*8) -1 :0];
+        4'b1000: r_y <= i_data[3][(LINE_SIZE_BYTES*8) -1 :0];
     endcase
 end
 endmodule
