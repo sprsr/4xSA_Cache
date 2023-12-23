d08a113d148e5c18e43aff5b543ff5a7e45c180b
sprsr <projecthourglass@protonmail.com>
2023-12-23 00:17:41 -0600

    comparator update

--- src/common/comparator/comparator.v
+++ src/common/comparator/comparator.v
@@ -5,13 +5,16 @@ module Comparator
 (
     input [TAG_WIDTH - 1: 0] i_a,
     input [TAG_WIDTH - 1: 0] i_b,
-    output reg               o_y
+    output reg               o_y = 1'b0
 );
+    initial begin
+        o_y <= 1'b0;
+    end
     always @(*) begin
         if (i_a == i_b)
             o_y <= 1'b1;
         else 
             o_y <= 1'b0;
-
+    end
 endmodule
 
