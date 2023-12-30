d59cb4ccb16267dbba2edb3d4e23df2e05d1b292
sprsr <projecthourglass@protonmail.com>
2023-12-30 00:07:48 -0600

    port adjustment

--- src/common/mux_4x1/mux_4x1.v
+++ src/common/mux_4x1/mux_4x1.v
@@ -8,7 +8,10 @@ module mux_4x1
    parameter TAG_BITS = 18
 )
 (
-  input [(VALID_BITS + LRU_BITS + DIRTY_BITS + TAG_BITS + (LINE_SIZE_BYTES * 8))- 1 : 0]  i_data [0: WAYS-1],
+  input [(VALID_BITS + LRU_BITS + DIRTY_BITS + TAG_BITS + (LINE_SIZE_BYTES * 8))- 1 : 0]  i_data_0,
+  input [(VALID_BITS + LRU_BITS + DIRTY_BITS + TAG_BITS + (LINE_SIZE_BYTES * 8))- 1 : 0]  i_data_1,
+  input [(VALID_BITS + LRU_BITS + DIRTY_BITS + TAG_BITS + (LINE_SIZE_BYTES * 8))- 1 : 0]  i_data_2,
+  input [(VALID_BITS + LRU_BITS + DIRTY_BITS + TAG_BITS + (LINE_SIZE_BYTES * 8))- 1 : 0]  i_data_3,
   input [WAYS - 1: 0]      i_sel,
   output [(LINE_SIZE_BYTES * 8) - 1 : 0] o_y 
 );
@@ -18,10 +21,10 @@ assign o_y = r_y;
 
 always @(*) begin
     case (i_sel) 
-        4'b0001: r_y <= i_data[0][(LINE_SIZE_BYTES*8) -1 :0];
-        4'b0010: r_y <= i_data[1][(LINE_SIZE_BYTES*8) -1 :0];
-        4'b0100: r_y <= i_data[2][(LINE_SIZE_BYTES*8) -1 :0];
-        4'b1000: r_y <= i_data[3][(LINE_SIZE_BYTES*8) -1 :0];
+        4'b0001: r_y <= i_data_0;
+        4'b0010: r_y <= i_data_1;
+        4'b0100: r_y <= i_data_2;
+        4'b1000: r_y <= i_data_3;
     endcase
 end
 endmodule
